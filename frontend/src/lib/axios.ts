998231c91419676f3d442927d87698281febf2f8
Gorkem95 <gorkem_095@hotmail.com>
2023-02-28 13:28:00 +0300

    Update

---
+++ frontend/src/lib/axios.ts
@@ -0,0 +1,17 @@
+import Axios from 'axios';
+
+let access_token = null;
+
+if (typeof window !== 'undefined') {
+    const user: any = window.localStorage.getItem('user');
+    access_token = JSON.parse(user)['access_token'];
+}
+
+const axios = Axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URI,
+    headers: {
+        Authorization: `Bearer ${access_token}`,
+    },
+});
+
+export default axios;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,17 @@
-
+import { useEffect, useState } from 'react';
+import axios from '@/lib/axios';
 
 export default function Home() {
-  return (
-      <>
-        <></>
-      </>
-  )
+    const [user, setUser] = useState({
+        name: undefined,
+    });
+
+    useEffect(() => {
+        axios.get('/auth/user').then(res => setUser(res.data));
+    }, []);
+    return (
+        <>
+            <h1 className="text-9xl text-red-700">{user.name}</h1>
+        </>
+    );
 }
